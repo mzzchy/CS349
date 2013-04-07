fe2d266cbe512b90ac4cf3d9b7440179488cdd7f
Yue <wei@v1424-wn-172-21-96-124.campus-dynamic.uwaterloo.ca>
2013-04-06 20:55:54 -0400

    File picker-> file parser

--- A5/src/com/example/ksmobile/MainActivity.java
+++ A5/src/com/example/ksmobile/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.ksmobile;
 
 
+import java.io.File;
+import java.io.IOException;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 //import android.content.Intent;
 import android.util.Log;
 import android.view.Menu;
@@ -35,8 +41,35 @@ public class MainActivity extends Activity implements ColorPickerDialog.OnColorC
 		
 	}
 	
+	public CharSequence[] listRaw(){
+		try {
+			return getBaseContext().getResources().getAssets().list("KSketch");
+//			for(String s: allFilesInPackage){
+//				Log.e("fileName", s);
+//			}
+			
+		} catch (IOException e) {
+			Log.e("ListFile","KSketch doesn't exist");
+		}
+		return null;
+	}
+	
 	public void fileButtonClicked(View view){
 		//SHow a new dialog
+		try {
+			final CharSequence[] items = getBaseContext().getResources().getAssets().list("KSketch");
+			AlertDialog.Builder builder = new AlertDialog.Builder(this);
+			builder.setTitle("Open file...");
+			builder.setItems(items, new DialogInterface.OnClickListener() {
+			    public void onClick(DialogInterface dialog, int item) {
+//			    	Log.e("Item", (String) items[item]);
+			    }
+			});
+			AlertDialog alert = builder.create();
+			alert.show();
+		} catch (IOException e) {
+			Log.e("ListFile","KSketch doesn't exist");
+		}
 	}
 
 	@Override
@@ -45,6 +78,5 @@ public class MainActivity extends Activity implements ColorPickerDialog.OnColorC
 		animeView.setBackgroundColor(color);
 		
 	}
-	
 
 }
